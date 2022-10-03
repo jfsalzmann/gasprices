3a295f8fb7d1b39f9c2c8fdfc1dafa614616eebb
Joff <mail@jfsalzmann.com>
2022-10-03 18:36:24 +0200

    gasprice

--- prep/eex_gas_cheated.R
+++ prep/eex_gas_cheated.R
@@ -1,4 +1,12 @@
 library(tidyverse)
 library(magrittr)
 
-data = read_csv("data-constr/558.csv")
+data558 = read_csv("data-constr/558.csv") %>% select(Gasprice,Gasday)
+
+data = read_csv("data-constr/Gas Price_DE.csv", 
+                         col_types = cols(Gasprice = col_double(),Gasday = col_date(format = "%d.%m.%y")))
+
+data_merged = data %>% bind_rows(data558) %>% filter(!is.na(Gasday))
+
+data_merged %>%
+  write_csv("data-constr/gasprice_imputed.csv")
