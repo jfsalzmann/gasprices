d4fcc8d1174d4c0c06dbfab49da0602b401dc404
Joff <mail@jfsalzmann.com>
2022-11-08 18:35:59 +0100

    the

--- prep/the.R
+++ prep/the.R
@@ -1,32 +1,31 @@
 library(xml2)
+library(XML)
 library(tidyverse)
 library(magrittr)
+library(httr)
 
-data = read_xml("data-orig/getXML.xml")
-data_x = data %>% as_list %>% as_tibble() %>% filter(DATA_id == "AggregatedConsumptionData")
+url = "https://datenservice.tradinghub.eu/XmlInterface/getXML.ashx?ReportId=AggregatedConsumptionData&Start=01-06-2020&End=31-12-2022"
+file_orig = "data-orig/the.xml"
+file_adj = "data-constr/the.xml"
+
+GET(url, write_disk(file_orig, overwrite=TRUE))
+
+cat("\n", file = file_orig, append = TRUE)
+
+readLines(file_orig) %>%
+  str_replace_all(
+    pattern = ' xmlns="urn:schemas-microsoft-com:sql:SqlRowSet1"', 
+    replace = "") %>%
+  str_replace_all(
+    pattern = '</AggregatedConsumptionData>$', 
+    replace = "</ACD>") %>%
+  str_replace_all(
+    pattern = '<AggregatedConsumptionData><xsd:', 
+    replace = "<ACD><xsd:") %>%
+  writeLines(file_adj)
 
 
-library(XML)
-install.packages("XML")
-doc = xmlParse("data-orig/getXML.xml")
-xmldf = xmlToDataFrame(nodes = getNodeSet(doc, "//AggregatedConsumptionData "))
-
-
-
-library(xml2)
-pg <- read_xml("data-orig/getXML.xml")
-
-# get all the <record>s
-recs <- xml_find_all(pg, "//AggregatedConsumptionData")
-
-# extract and clean all the columns
-vals <- trimws(xml_text(recs))
-
-# extract and clean (if needed) the area names
-labs <- trimws(xml_attr(recs, "label"))
-cols <- xml_attr(xml_find_all(pg, "//data/variables/*[self::categoricalvariable or
-                                                      self::realvariable]"), "name")
-dat <- do.call(rbind, lapply(strsplit(vals, "\ +"),
-                             function(x) {
-                               data.frame(rbind(setNames(as.numeric(x),cols)))
-                             }))
+data = read_xml(file_adj) %>%
+  xmlParse() %>%
+  getNodeSet("//AggregatedConsumptionData") %>%
+  xmlToDataFrame()
