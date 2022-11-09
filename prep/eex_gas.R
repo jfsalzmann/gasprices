687396fe47fdf9196576ba20df9dcfb559606556
Joff <mail@jfsalzmann.com>
2022-11-09 12:07:47 +0100

    eex ngp

--- prep/eex_gas.R
+++ prep/eex_gas.R
@@ -2,10 +2,26 @@ library(tidyverse)
 library(magrittr)
 
 library(jsonlite)
-library(data.table)
 
-url = readLines("https://www.powernext.com/data-feed/132735/139/558")
+library(rvest)
+library(xml2)
+library(anytime)
 
-json = fromJSON(url[0])
+# url = readLines("https://www.powernext.com/data-feed/132735/139/558")
 
-#%>% as.data.frame
+
+url = "https://www.powernext.com/data-feed/1467707/819/17"
+file_orig = "data-orig/eex_ngp.json"
+GET(url, write_disk(file_orig, overwrite=TRUE))
+cat("\n", file = file_orig, append = TRUE)
+
+
+data_eex_ngp = read_html(file_orig) %>%
+  html_text() %>%
+  fromJSON() %$% values$data[[1]] %>%
+  rename(date = x, ngp = y) %>%
+  select(-name) %>%
+  mutate(date = anydate(date/1000))
+
+
+save(data_eex_ngp,file="data-constr/eex_ngp.RData")
